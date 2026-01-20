3c70fd663646bb59a21812ed6c1652c7eb9f521a
disha <disha.acharya1111@gmail.com>
2026-01-20 23:16:12 +0530

    Added full stack folder

---
+++ pr/script.js
@@ -0,0 +1,13 @@
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 200) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+
+topBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+};
